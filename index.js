0504ccf573075e22bdfdcdb4cfea901b82348141
Dezmou <incredibledez@gmail.com>
2022-10-12 14:30:35 +0200

    wip

--- index.js
+++ index.js
@@ -1,48 +1,91 @@
 const WHITE = 0;
 const BLACK = 1;
 
-const getBoard = () => {
+
+
+; (async () => {
   const board = Array.from({ length: 8 }).map(e => Array.from({ length: 8 }).map(e => ({
     piece: "blank",
     color: -1,
     child: null,
+    hud: null,
   })))
-  const boardEl = document.querySelector("#main-wrap > main > div.analyse__board.main-board > div.cg-wrap.cgv1.orientation-white.manipulable > cg-container > cg-board");
-  const width = boardEl.clientWidth;
-  const height = boardEl.clientHeight;
-  for (let child of boardEl.childNodes) {
-    if (child.className.indexOf("last-move") !== -1) continue;
-    const spClass = child.className.split(" ");
-    const color = { black: BLACK, white: WHITE }[spClass[0]];
-    const type = spClass[1];
-    const pos = child.style.transform.split("(")[1].split(")")[0].split(",").map(e => parseInt(e.replace("px", "").trim()))
-    const x = Math.floor(pos[0] / width / 0.125);
-    const y = Math.floor(pos[1] / height / 0.125);
-    board[y][x] = { piece: type, color, child };
-  }
-  return board;
-};
 
-const printboard = (board) => {
-  let final = ""
+  const chien = document.querySelector("#main-wrap > main > div.analyse__board.main-board > div.cg-wrap.cgv1.orientation-white.manipulable > cg-container > cg-board");
+  const container = document.createElement("div");
+  container.style.width = `${chien.clientWidth}px`;
+  container.style.height = `${chien.clientHeight}px`;
+  container.style["pointer-events"] = `none`;
+  // container.style.background = "red";
+  container.style["z-index"] = "8888";
+  container.style.transform = "rotate(0deg)";
+
   for (let line of board) {
-    let str = ""
+    const divLine = document.createElement("div");
+    divLine.style.with = "100%";
+    divLine.style.display = "flex";
+    container.appendChild(divLine);
     for (let point of line) {
-      if (point.piece !== "blank") {
-        str += " X "
-      } else {
-        str += " . "
+      const hud = document.createElement("div");
+      hud.style.width = `${chien.clientWidth / 8}px`;
+      hud.style.height = `${chien.clientHeight / 8}px`;
+      hud.style.outline = "1px solid blue";
+      point.hud = hud;
+      divLine.appendChild(hud)
+    }
+  }
+  chien.appendChild(container)
+
+  const getBoard = () => {
+    const boardEl = document.querySelector("#main-wrap > main > div.analyse__board.main-board > div.cg-wrap.cgv1.orientation-white.manipulable > cg-container > cg-board");
+    const width = boardEl.clientWidth;
+    const height = boardEl.clientHeight;
+    for (let child of boardEl.childNodes) {
+      if (child.className.indexOf("last-move") !== -1) continue;
+      const spClass = child.className.split(" ");
+      const color = { black: BLACK, white: WHITE }[spClass[0]];
+      const type = spClass[1];
+      const pos = child.style.transform.split("(")[1].split(")")[0].split(",").map(e => parseInt(e.replace("px", "").trim()))
+      const x = Math.floor(pos[0] / width / 0.125);
+      const y = Math.floor(pos[1] / height / 0.125);
+      board[y][x] = { piece: type, color, child };
+    }
+    return board;
+  };
+
+
+  const printboard = () => {
+    let final = ""
+    for (let line of board) {
+      let str = ""
+      for (let point of line) {
+        if (point.piece !== "blank") {
+          str += " X "
+        } else {
+          str += " . "
+        }
+      }
+      final += `${str}\n`
+    }
+    console.log(final);
+  };
+
+  const analyse = () => {
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        const piece = board[y][x];
+        if (piece.piece === "pawn") {
+
+        }
       }
     }
-    final += `${str}\n`
-  }
-  console.log(final);
-};
+  };
 
-; (async () => {
+
   while (true) {
-    const board = getBoard();
-    printboard(board);
-    await new Promise(r => setTimeout(r, 500));
+    getBoard();
+    // printboard(board);
+    // analyse(board);
+    await new Promise(r => setTimeout(r, 1000));
   }
 })()
